77fe7e8f0edd574362b9126e65ad7d1868ea0e68
Dreamerc1 <chelsreid99@gmail.com>
2024-11-06 14:30:37 +0000

    final commit for feature branch

--- ui-front-end/src/components/market-depth/QuantityCell.tsx
+++ ui-front-end/src/components/market-depth/QuantityCell.tsx
@@ -1,21 +1,4 @@
-/* import React from 'react';
-import './QuantityCell.css';
 
-export interface QuantityCellProps {
-  quantity: number;
-  type: 'bid' | 'ask';
-}
-
-export const QuantityCell = (props: QuantityCellProps) => {
-  const { quantity, type } = props;
-
-  return (
-    <td className={`QuantityCell ${type}`}>
-      <div className="bar" style={{ width: `${Math.min(100, (quantity / 2000) * 100)}%` }}></div>
-      {quantity}
-    </td>
-  );
-}; */
 import React from 'react';
 import './QuantityCell.css';
 
